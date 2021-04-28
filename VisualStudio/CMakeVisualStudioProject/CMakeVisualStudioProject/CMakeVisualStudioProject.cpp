9b1f54e5e4c8cc07dd695d6cede3356782cdce78
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-04-27 21:49:10 -0500

    Visual Studio Startup CMake project

---
+++ VisualStudio/CMakeVisualStudioProject/CMakeVisualStudioProject/CMakeVisualStudioProject.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeVisualStudioProject.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeVisualStudioProject.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
